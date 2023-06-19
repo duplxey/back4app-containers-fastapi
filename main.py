b557cdf346b017c0e8ce6f5ddb14a4a84f84c7d7
Nik Tomazic <nick.tomazic@gmail.com>
2023-06-19 16:43:27 +0200

    added /cat-fact endpoint

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Back4app Containers rocks!"}
+
+
+@app.get("/cat-fact")
+def cat_fact():
+    response = requests.get("https://catfact.ninja/fact")
+    return {"fact": response.json()["fact"]}
